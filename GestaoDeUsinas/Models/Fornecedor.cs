3f82d217256daa7b0d78462b6ecd3957bd694e02
Diego <diego_sousa.s@outlook.com>
2021-06-25 22:14:29 -0300

    Definindo models do projeto

---
+++ GestaoDeUsinas/Models/Fornecedor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeUsinas.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+    }
+}
---
+++ GestaoDeUsinas/Models/Usina.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeUsinas.Models
+{
+    public class Usina
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int FornecedorId { get; set; }
+
+        //Join com a entidade de fornecedor
+        public Fornecedor Fornecedor { get; set; }
+
+        [Required]
+        public string UCusina { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
